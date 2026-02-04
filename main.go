fb77002205233850c2e9ed3d835310890742ed20
523h0102 <523h0102@student.tdtu.edu.vn>
2026-02-04 11:31:50 +0700

    change window to popup

--- main.go
+++ main.go
@@ -13,32 +13,32 @@ import (
 	//GUI
 	"github.com/mattn/go-gtk/gdkpixbuf"
 	"github.com/mattn/go-gtk/glib"
-	"github.com/mattn/go-gtk/gtk"	
+	"github.com/mattn/go-gtk/gtk"
 )
 
 const (
-	DIR string = "/home/thuan/Pictures/Wallpapers/"
-	SCREEN_WIDTH int32 = 800
-	SCREEN_HEIGHT int32 = 450
-	MAX_IMG int = 6
+	DIR           string = "/home/thuan/Pictures/Wallpapers/"
+	SCREEN_WIDTH  int32  = 800
+	SCREEN_HEIGHT int32  = 450
+	MAX_IMG       int    = 3
 )
 
 var (
-	IMG_EXT = [...]string{".jpg", ".jpeg", ".png", ".gif", ".svg", ".bmp", ".tiff", ".tif", ".webp", ".heic", ".heif", ".avif"}
-	images = []string{}
+	IMG_EXT     = [...]string{".jpg", ".jpeg", ".png", ".gif", ".svg", ".bmp", ".tiff", ".tif", ".webp", ".heic", ".heif", ".avif"}
+	images      = []string{}
 	images_name = []string{}
 )
 
-func isImage(file_name string) bool{
-	for _, ext := range(IMG_EXT) {
-		if strings.HasSuffix(file_name, ext){
+func isImage(file_name string) bool {
+	for _, ext := range IMG_EXT {
+		if strings.HasSuffix(file_name, ext) {
 			return true
 		}
 	}
 	return false
 }
 
-func switchWallpaper(file_path string) error{
+func switchWallpaper(file_path string) error {
 	cmd := exec.Command("swww", "img", file_path, "--transition-type", "random")
 
 	err := cmd.Run()
@@ -48,7 +48,6 @@ func switchWallpaper(file_path string) error{
 	return nil
 }
 
-
 func main() {
 
 	files, err := os.ReadDir(DIR)
@@ -57,15 +56,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for _, file := range(files) {
+	for _, file := range files {
 		file_name := file.Name()
 		if !isImage(file_name) {
 			log.Fatal(errors.New("contains file(s) is not image"))
 		}
 		images_name = append(images_name, file_name)
-		images = append(images, DIR + file_name)
+		images = append(images, DIR+file_name)
 	}
-
 
 	file, err := os.Open(images[0])
 	if err != nil {
@@ -86,14 +84,14 @@ func main() {
 
 	gtk.Init(&os.Args)
 
-	window := gtk.NewWindow(gtk.WINDOW_TOPLEVEL)
+	window := gtk.NewWindow(gtk.WINDOW_POPUP)
 	window.SetPosition(gtk.WIN_POS_CENTER)
-	window.SetTitle("GTK Go!")
+	window.SetTitle("gows")
 	window.SetIconName("gtk-dialog-info")
 	window.Connect("destroy", func(ctx *glib.CallbackContext) {
 		fmt.Println("got destroy!", ctx.Data().(string))
 		gtk.MainQuit()
-	}, "foo")
+	}, "foo")	
 	scrolledWindow := gtk.NewScrolledWindow(nil, nil)
 
 	vbox := gtk.NewVBox(false, 1)
@@ -106,7 +104,7 @@ func main() {
 
 	frame := gtk.NewFrame("gows wallpaper switcher")
 	framebox1 := gtk.NewVBox(false, 0)
-	
+
 	frame.Add(framebox1)
 
 	vpaned.Pack1(frame, false, false)
@@ -115,20 +113,20 @@ func main() {
 
 	row := gtk.NewHBox(false, 0)
 	wallpapers := gtk.NewVBox(false, 0)
-	
+
 	count := 0
-	for i, _ := range(images) {
+	for i, _ := range images {
 		button := gtk.NewButtonWithLabel("")
-	
+
 		pixbuf, err := gdkpixbuf.NewPixbufFromFile(images[i])
 		if err != nil {
 			log.Fatal(err)
 		}
 		pixbuf = pixbuf.ScaleSimple(192, 180, gdkpixbuf.INTERP_BILINEAR)
-		
+
 		image := gtk.NewImage()
 		image.SetFromPixbuf(pixbuf)
-		
+
 		button.SetImage(image)
 		button.Clicked(func() {
 			err := switchWallpaper(images[i])
@@ -139,8 +137,8 @@ func main() {
 		row.Add(button)
 		wallpapers.Add(row)
 
-		if count == MAX_IMG {
-				row = gtk.NewHBox(false, 0)
+		if count == MAX_IMG-1 {
+			row = gtk.NewHBox(false, 0)
 			count = 0
 			continue
 		}
